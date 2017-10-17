d6b8057d9e7159147ca5d95b36fe10b064ded2d3
Blessing Ebowe <blessingebowe@gmail.com>
2017-10-17 18:26:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,9 @@ function checkCollision(rock) {
      * 3. The rock's left edge is < the DODGER's right edge,
      *    and the rock's right edge is > the DODGER's right edge
      */
-    if (rockLeftEdge > dodgerLeftEdge && rockLefttEdge < dodgerRightEdge) {
+    if ((rockLeftEdge > dodgerLeftEdge && rockLefttEdge < dodgerRightEdge) ||
+        (rockRightEdge < dodgerRightEdge && rockRightEdge > rockLeftEdge)
+        ) {
       return true
     } else {
       return false
